dfba7bfc3f1cabcf0f0ab762e64f6de488d4adf0
unravel123 <anomynous992@gmail.com>
2020-10-23 14:20:35 +0700

    add home page and book list page

--- TestAsync.js
+++ TestAsync.js
@@ -2,14 +2,14 @@ const async = require('async');
 
 async.parallel({
     one: function(callback) {
-        setTimeout(function() {
+    
             callback(null, 1);
-        }, 200);
+ 
     },
     two: function(callback) {
-        setTimeout(function() {
+     
             callback(null, 2);
-        }, 100);
+       
     }
 }, function(err, results) {
     // results is now equals to: {one: 1, two: 2}
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,11 +1,44 @@
 const Book = require('../models/book');
+const Author = require('../models/author');
+const Genre = require('../models/genre');
+const BookInstance = require('../models/bookinstance');
+
+const async = require('async');
 
 exports.index = function(req, res) {
-    res.send('NOT IMPLEMENTED: Site Home Page');
+
+    async.parallel({
+        book_count: function(callback) {
+            Book.countDocuments({}, callback);
+        },
+        book_instance_count: function(callback) {
+            BookInstance.countDocuments({}, callback);
+        },
+        book_instance_available_count: function(callback) {
+            BookInstance.countDocuments({ status: 'Available' }, callback);
+        },
+        authot_count: function(callback) {
+            Author.countDocuments({}, callback);
+        },
+        genre_count: function(callback) {
+            Genre.countDocuments({}, callback);
+        },
+    }, function(err, result){
+        res.render('index', { title: 'Local Library Home', error: err, data: result});
+    });
+    
 };
 
 exports.book_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: Book list');
+    
+    Book.find({}, 'title author')
+        .populate('author')
+        .exec(function (err, books){
+            if(err) return async.next(err);
+
+            res.render('book_list', { title: 'Book List', books: books });
+        });
+
 };
 
 exports.book_detail = function(req, res) {
