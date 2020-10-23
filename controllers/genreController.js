accbe461a4066f86a17e9aed08491f5071bb5edb
unravel123 <anomynous992@gmail.com>
2020-10-23 11:39:05 +0700

    remove gitignore files

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,5 @@
 const Genre = require('../models/genre');
+const { body, validator, validationResult } = require('express-validator');
 
 exports.genre_list = function(req, res) {
     res.send('NOT IMPLEMENTED: Genre list');
@@ -9,12 +10,42 @@ exports.genre_detail = function(req, res) {
 };
 
 exports.genre_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create GET');
+    res.render('genre_form', { title: 'Create Genre' });
 };
 
-exports.genre_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre create POST');
-};
+exports.genre_create_post = [
+    body('name', 'Genre name required').trim().isLength({ min: 1 }).escape(),
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        let genre = new Genre(
+            { name: req.body.name }
+        );
+
+        //handle errors
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Create Genre', genre: genre, errors: errors.array()});
+            return;
+        } else {
+            //check exists
+            Genre.findOne({ 'name': req.body.name })
+                .exec(function(err, isFound) {
+                    if(err) return next(err);
+
+                    if(isFound) {
+                        console.log(isFound.url)
+                        res.redirect(isFound.url);
+                    } else {
+                        genre.save(function(err) {
+                            if (err) return next(err);
+                            console.log(genre.url)
+                            res.redirect(genre.url);
+                        });
+                    }
+                });
+        }
+    }
+];
 
 exports.genre_delete_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Genre delete GET');
