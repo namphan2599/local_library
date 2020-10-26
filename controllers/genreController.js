407a7d822f1aaee1d195bbc76a22455e5d88e1f6
unravel123 <anomynous992@gmail.com>
2020-10-26 15:00:45 +0700

    add genre detail page

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,4 +1,8 @@
 const Genre = require('../models/genre');
+const Book = require('../models/book');
+
+const async = require('async');
+
 const { body, validator, validationResult } = require('express-validator');
 
 exports.genre_list = function(req, res, next) {
@@ -10,8 +14,29 @@ exports.genre_list = function(req, res, next) {
         });
 };
 
-exports.genre_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Genre detail: ' + req.params.id);
+exports.genre_detail = function(req, res, next) {
+
+
+    async.parallel({
+        genre: function(callback) {
+            Genre.findById(req.params.id)
+                .exec(callback);
+        },
+        genre_books: function(callback) {
+            Book.find({ 'genre': req.params.id })
+                .exec(callback);
+        }
+    }, function(err, results) {
+        if (err) return next(err);
+        if (results.genre === null) {
+            let err = new Error('Genre not found');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('genre_detail', { title: 'Genre detail', genre: results.genre, genre_books: results.genre_books });
+    });
+
 };
 
 exports.genre_create_get = function(req, res) {
